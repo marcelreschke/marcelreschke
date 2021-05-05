759a4f090a3d804b938725f5272e6475bcacbcec
Marcel Reschke <hello@marcelreschke.com>
2021-05-05 17:41:32 +0200

    📦 NEW: Add clear-any-console

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+const clearConsole = require('clear-any-console');
+
+// Clears the console.
+clearConsole();
+
 console.log('Marcel Reschke - Digital Strategies as a Service!');
