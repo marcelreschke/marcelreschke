590a3fb83d024a37709b7d35031d0231c9a36064
Marcel Reschke <hello@marcelreschke.com>
2021-05-05 18:51:48 +0200

    👌 IMPROVE: swap clear-any-console for cli-welcome

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-// Clears the console.
-clearConsole();
+welcome({
+    title: pkgJSON.name,
+    tagLine: 'Cheers mate!',
+    version: pkgJSON.version,
+    description: pkgJSON.description,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+})
 
 console.log('Marcel Reschke - Digital Strategies as a Service!');
